bbc531056dbf766fc661918ab878cd410076d14a
xyndall <tyrel.ratima@gmail.com>
2024-07-20 16:45:50 +1200

    GameManagerCreatedUpdated input actions aswell
    move your branch here

--- Assets/Tyrel/UIAssets/ShadowInteract.cs
+++ Assets/Tyrel/UIAssets/ShadowInteract.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ShadowInteract : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public PlayerInputActions playerInputActions;
+    public GameObject radialMenu;
+    bool isRadialMenuOpen;
+
+    public Animator animator;
+
+    private void Awake()
     {
-        
+        playerInputActions = new PlayerInputActions();
+        radialMenu.SetActive(false);
+        isRadialMenuOpen = false;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnEnable()
     {
-        
+        playerInputActions.Enable();
+        playerInputActions.Player.Interact.performed += Interact;
     }
+
+    private void OnDisable()
+    {
+        playerInputActions.Disable();
+        playerInputActions.Player.Interact.performed += Interact;
+    }
+
+
+    void Interact(InputAction.CallbackContext context)
+    {
+        if (GameManager.instance.gameIsPaused == false )// && also check if over the shadow object
+        {
+            
+
+            if (isRadialMenuOpen && context.performed)
+            {
+                CloseRadialMenu();
+            }
+            else
+            {
+                OpenRadialMenu();
+            }
+
+        }
+    }
+
+    void OpenRadialMenu()
+    {
+        radialMenu.SetActive(true);
+        isRadialMenuOpen = true;
+    }
+    void CloseRadialMenu()
+    {
+        animator.SetTrigger("CloseRadialMenu");
+        isRadialMenuOpen = false;
+    }
+
+    
+
+
 }
